91c46caad31315824927b8c264dc082f5f731123
LINO ADRIÁN PÁEZ REYES <54821048+Adriianpr919@users.noreply.github.com>
2022-10-24 16:13:23 -0500

    varios cambios.

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,11 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: 'Adrian Paez.',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+      'Adrian Paez es un DISEÑO TU PÁGINA WEB ULTIMA Tecnologías Web FRONT-END DEVELOPER&reg;. que se especializa en construir (y ocasionalmente diseñar) experiencias digitales excepcionales.',
+    siteUrl: 'https://www.apr1991lino.vercel.app', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -20,8 +19,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'BrittanyChiang',
-        short_name: 'BrittanyChiang',
+        name: 'AdrianPaez.',
+        short_name: 'AdrianPaez.',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
